e35f339e744c12bf494fe4cce7f1ff0799330252
Chris5477 <petitcolaschristopher@gmail.com>
2022-09-20 13:57:04 +0200

    add test2

---
+++ test2.php
@@ -0,0 +1,12 @@
+<?php
+        $destination = array(
+            "France" => "Paris",
+            "Allemagne" => "Berlin",
+            "Italie" => "Rome",
+        );
+
+        foreach($destination as $capital => $test){
+            echo "$capital : $test ". PHP_EOL;
+        }
+
+?>
